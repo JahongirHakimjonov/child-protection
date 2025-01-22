c605f20e1b91ec94f6184718aebc0ecf6519c693
JahongirHakimjomov <jahongirhakimjonov@gmail.com>
2025-01-22 16:28:01 +0500

    feat: course, question chat add

--- apps/chat/admin/chat.py
+++ apps/chat/admin/chat.py
@@ -9,6 +9,7 @@ class ChatRoomAdmin(ModelAdmin):
     list_display = ("user", "created_at", "updated_at")
     search_fields = ("user__username",)
     list_filter = ("user",)
+    autocomplete_fields = ("user",)
 
 
 @admin.register(Message)
@@ -16,6 +17,7 @@ class MessageAdmin(ModelAdmin):
     list_display = ("chat_room", "sender", "created_at", "updated_at")
     search_fields = ("chat_room__user__username", "sender__username")
     list_filter = ("chat_room", "sender")
+    autocomplete_fields = ("chat_room", "sender")
 
     def get_queryset(self, request):
         return super().get_queryset(request).select_related("chat_room", "sender")
