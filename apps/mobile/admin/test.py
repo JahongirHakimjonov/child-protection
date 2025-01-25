713ed84935fe563225c735c7c73e7c10859d14ee
JahongirHakimjomov <jahongirhakimjonov@gmail.com>
2025-01-25 16:32:57 +0500

    feat: test add

--- apps/mobile/admin/test.py
+++ apps/mobile/admin/test.py
@@ -9,6 +9,7 @@ class AnswerAdmin(ModelAdmin):
     list_display = ["id", "answer", "type"]
     search_fields = ["answer", "type"]
     list_filter = ["type"]
+    autocomplete_fields = ["question"]
 
 
 @admin.register(TestQuestion)
@@ -16,6 +17,7 @@ class TestQuestionAdmin(ModelAdmin):
     list_display = ["id", "test", "question"]
     search_fields = ["test", "question"]
     list_filter = ["test"]
+    autocomplete_fields = ["test"]
 
 
 @admin.register(Test)
@@ -23,3 +25,4 @@ class TestAdmin(ModelAdmin):
     list_display = ["id", "title", "description"]
     search_fields = ["title", "description"]
     list_filter = ["title"]
+    autocomplete_fields = ["lesson"]
