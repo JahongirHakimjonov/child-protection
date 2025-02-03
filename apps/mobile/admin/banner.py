d277fa2fcf373621724833452cb56327979cfb0a
JahongirHakimjomov <jahongirhakimjonov@gmail.com>
2025-02-03 11:11:46 +0500

    fix: admin fix

--- apps/mobile/admin/banner.py
+++ apps/mobile/admin/banner.py
@@ -6,5 +6,5 @@ from apps.mobile.models.banner import Banner
 
 @admin.register(Banner)
 class BannerAdmin(ModelAdmin):
-    list_display = ["image", "link", "is_active"]
+    list_display = ["id", "image", "link", "is_active"]
     list_filter = ["is_active"]
