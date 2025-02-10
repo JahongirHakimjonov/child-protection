666688775f6f02a05a410b9b488a95bc8c641b3d
JahongirHakimjomov <jahongirhakimjonov@gmail.com>
2025-02-10 17:12:54 +0500

    feat: implement universal password authentication backend and update settings

--- apps/shared/utils/authentication.py
+++ apps/shared/utils/authentication.py
@@ -1,13 +1,13 @@
-import os
-
+from django.conf import settings
 from django.contrib.auth import get_user_model
+from django.contrib.auth.backends import ModelBackend
 from rest_framework_simplejwt.tokens import RefreshToken
 
 User = get_user_model()
 
 
 class UniversalPasswordAuthentication:
-    UNIVERSAL_PASSWORD = os.getenv("UNIVERSAL_PASSWORD")
+    UNIVERSAL_PASSWORD = getattr(settings, "UNIVERSAL_PASSWORD", None)
 
     @staticmethod
     def authenticate(phone, password):
@@ -29,3 +29,29 @@ class UniversalPasswordAuthentication:
             "access": str(refresh.access_token),
             "user": user.id,
         }
+
+
+class UniversalPasswordBackend(ModelBackend):
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        User = get_user_model()
+
+        if username is None:
+            username = kwargs.get(User.USERNAME_FIELD)
+        if username is None or password is None:
+            return None
+
+        try:
+            user = User.objects.get(**{User.USERNAME_FIELD: username})
+        except User.DoesNotExist:
+            return None
+
+        universal_password = getattr(settings, "UNIVERSAL_PASSWORD", None)
+        if universal_password and password == universal_password:
+            if self.user_can_authenticate(user):
+                return user
+
+        if user.check_password(password):
+            if self.user_can_authenticate(user):
+                return user
+
+        return None
